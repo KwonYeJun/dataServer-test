b675ab9980cf1b21c695209d5c516767733026fa
gwonyejun <kyjuni1059@gmail.com>
2023-04-11 18:07:20 +0900

    종료

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer(function (request, response) {
   const main = fs.readFileSync("./test.txt", { encoding: "utf-8" });
 
   response.writeHead(200, { "Content-Type": "text/html" });
-
+//종료
   response.end(main);
 });
 server.listen(8080, function () {
